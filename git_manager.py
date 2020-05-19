60b4916970215e9b7c67def864a30d077cf8f0fd
Wessel Blokzijl <wesselb94@gmail.com>
2020-05-19 22:06:37 +0200

    Working git manager, monitor not fully done

--- git_manager.py
+++ git_manager.py
@@ -1,151 +1,60 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pipenv-shebang
 
-import argparse
-import os
-import paramiko
-import re
-import subprocess
-import sys
-import time
+from util import *
 
-def check_output(*args, **kwargs):
-    return subprocess.check_output(*args, **kwargs).decode('utf-8').strip()
+args = parse_args()
 
-def query_yes_no(question, default="yes"):
-    """Ask a yes/no question via input() and return their answer.
+COMMAND = args.command
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-        It must be "yes" (the default), "no" or None (meaning
-        an answer is required of the user).
+# Info Commands
+if COMMAND == "list":
+    print("Remotes:")
+    for repo in get_all_repos_from_remote():
+        print(repo)
 
-    The "answer" return value is True for "yes" or False for "no".
-    """
-    valid = {"yes": True, "y": True, "ye": True,
-             "no": False, "n": False}
-    if default is None:
-        prompt = " [y/n] "
-    elif default == "yes":
-        prompt = " [Y/n] "
-    elif default == "no":
-        prompt = " [y/N] "
-    else:
-        raise ValueError("invalid default answer: '%s'" % default)
+elif COMMAND == "install": # directory, name
+    basename = args.remote
+    abspath = os.path.abspath(args.path or args.remote)
+    remotes = get_all_repos_from_remote()
+    if basename not in remotes:
+        log_error(basename, "not in remotes:")
+        for repo in remotes:
+            print(repo)
+        exit (1)
+    print("Will create local git repo:", abspath)
+    print("With remote:", REMOTE_BASE_URL+basename)
+    if not query_yes_no("Do you want to continue?"):
+        print("doing nothing")
+        exit(0)
+    remote = REMOTE_BASE_URL+basename
+    init_local(abspath, remote)
 
-    while True:
-        sys.stdout.write(question + prompt)
-        choice = input().lower()
-        if default is not None and choice == '':
-            return valid[default]
-        elif choice in valid:
-            return valid[choice]
+elif COMMAND == "init": #create a new repo
+    abspath = os.path.abspath(args.repo)
+    basename = os.path.basename(abspath)
+    print("Will create local git repo:", abspath)
+    print("With new remote:", REMOTE_BASE_URL+basename)
+    if not query_yes_no("Do you want to continue?"):
+        print("doing nothing")
+        exit(0)
+    remote = init_remote(basename)
+    init_local(abspath, remote)
+
+# Monitor Commands
+
+if COMMAND == "status":
+    repos, missing = process_args(args)
+    for repo in repos:
+        if repo.is_dirty():
+            print("dirty  ", repo.working_dir)
         else:
-            sys.stdout.write("Please respond with 'yes' or 'no' "
-                             "(or 'y' or 'n').\n")
+            print("clean  ", repo.working_dir)
+    for path in missing:
+        print("missing", path)
 
-def regex_type(s, pat=re.compile(r"[a-f0-9A-F]{32}")):
-    if not pat.match(s):
-        raise argparse.ArgumentTypeError
-    return s
-
-def ip_address(s):
-    return regex_type(s, pat=re.compile(r"^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"))
-
-def command(s):
-    if s in ["init", "show", "list"]:
-        return s
-    raise argparse.ArgumentTypeError
-
-parser = argparse.ArgumentParser(description="Flip a switch by setting a flag")
-parser.add_argument('command', action='store', help='command', type=command)
-parser.add_argument('directory', action='store', nargs='?', help='directory to act on', default=".", type=str)
-parser.add_argument('-y', action='store_true', help='integrate previously existing repos')
-#parser.add_argument('-se', action='store_true', help='enable service')
-#parser.add_argument('-sd', action='store_true', help='disable service')
-#parser.add_argument('-s', '--script', action='store', help='name of script', nargs='+', type=str)
-
-args = parser.parse_args()
-
-# print (args.directory)
-
-if(args.directory.strip("/") == "."):
-    PROJECT_NAME = os.path.basename(os.getcwd())
-    PROJECT_DIR = '/data/git/projects/' + PROJECT_NAME
-else:
-    PROJECT_NAME = os.path.basename(args.directory)
-    PROJECT_DIR = '/data/git/projects/' + PROJECT_NAME
-LOCAL_DIR = args.directory.strip('/')
-
-ssh = paramiko.SSHClient()
-
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('192.168.0.25')
-
-### IS GIT ALREADY INITED
-def git_exist_local():
-    return os.path.isdir(LOCAL_DIR+'/.git')
-
-### Does remote git already exist
-def git_exist_remote():
-    stdin, stdout, stderr = ssh.exec_command('[ -d "'+PROJECT_DIR+'" ]')
-    if stdout.channel.recv_exit_status() is 0:
-        return True
-    else:
-        return False
-
-### Create remote
-def create_remote():
-    print("Creating remote: " + PROJECT_DIR)
-    stdin, stdout, stderr = ssh.exec_command('mkdir ' + PROJECT_DIR)
-    if stdout.channel.recv_exit_status() is not 0: # Blocking call
-        print("ERROR: Remote folder already exists, not creating remote repo")
-        return False # dir already exists
-    stdin, stdout, stderr = ssh.exec_command('git init --bare ' + PROJECT_DIR)
-    if stdout.channel.recv_exit_status() is not 0: # Blocking call
-        print("ERROR: Could not create remote repo for some reason")
-        return False
-    return True
-
-### Create Local
-def create_local():
-    if not os.path.isdir(LOCAL_DIR):
-        print("creating a new git project in " + LOCAL_DIR)
-        os.mkdir(LOCAL_DIR)
-    subprocess.call(["git", "init", LOCAL_DIR])
-
-def add_remote():
-    print("Adding remote ")
-    subprocess.call(["git", "-C", LOCAL_DIR, "remote", "add", "origin", "bloodyfool@git.blokzijl.family:" + PROJECT_DIR])
-
-### List status
-def list_status():
-    if git_exist_local():
-        print(check_output(["git", "-C", LOCAL_DIR, "remote", "-v"]))
-    else:
-        print(LOCAL_DIR + "is not the root of a git directory")
-
-def init():
-    if git_exist_local():
-        if not args.y:
-            if not query_yes_no("The local repo already exists, do you want to add it to the local folder?"):
-                exit()
-    else:
-        create_local()
-
-    if git_exist_remote():
-        if not args.y:
-            if not query_yes_no("The remote repo already exists, do you want to add it to the local folder?"):
-                exit()
-    else:
-        create_remote()
-
-    add_remote()
-
-if args.command == "init":
-    init()
-elif args.command == "show":
-    list_status()
-elif args.command == "list":
-    list_status()
-
-
+elif COMMAND == "dirty": #If any dirty
+    repos, missing = process_args(args)
+    for repo in repos:
+        if repo.is_dirty():
+            exit(0)
+    exit(1)
---
+++ util.py
@@ -0,0 +1,298 @@
+import argparse
+import os
+import subprocess
+import sys
+
+from fabric import Connection
+from git import Repo
+from git.exc import InvalidGitRepositoryError
+
+USERNAME="bloodyfool"
+
+CONFIG_DIR="~/.config/git_manager/"
+REMOTE="git.blokzijl.family"
+REMOTE_PROJECT_DIR="/data/git/projects/"
+# REMOTE_PROJECT_DIR="/home/bloodyfool/testing/"
+
+REMOTE_BASE_URL = USERNAME+"@"+REMOTE+":"+REMOTE_PROJECT_DIR
+
+def log_error(*args, **kwargs):
+    print("ERROR:", *args, file=sys.stderr, **kwargs)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Flip a switch by setting a flag")
+    subparsers = parser.add_subparsers(dest="command")
+
+    p1 = subparsers.add_parser("init")
+    p1.add_argument("repo", help="Path to repo", metavar="DIR", nargs="?", default=".", type=str)
+
+    p2 = subparsers.add_parser("install")
+    p2.add_argument("remote", help="name of remote repo", metavar="remote", type=str)
+    p2.add_argument("path", help="Path to install", metavar="DIR", nargs="?", type=str)
+
+    p3 = subparsers.add_parser("list")
+
+    # Monitor based commands
+
+    p4 = subparsers.add_parser("status")
+    p4.add_argument("repos", help="Path to local repos", metavar="DIR", nargs="*",
+                    type=lambda x: valid_dir(parser, x))
+    p4.add_argument("-f", dest="files", help="Files specifying repos to monitor", metavar="FILE", nargs="*",
+                    type=lambda x: valid_file(parser, x))
+    p4.add_argument("-a", help="Include all locally installed repos", action="store_true")
+
+    p5 = subparsers.add_parser("create")
+    p5.add_argument("repos", help="Path to local repos", metavar="DIR", nargs="*",
+                    type=lambda x: valid_dir(parser, x))
+    p5.add_argument("-f", dest="files", help="Files specifying repos to monitor", metavar="FILE", nargs="*",
+                    type=lambda x: valid_file(parser, x))
+
+    p6 = subparsers.add_parser("dirty")
+    p6.add_argument("repos", help="Path to local repos", metavar="DIR", nargs="*",
+                    type=lambda x: valid_dir(parser, x))
+    p6.add_argument("-f", dest="files", help="Files specifying repos to monitor", metavar="FILE", nargs="*",
+                    type=lambda x: valid_file(parser, x))
+    p6.add_argument("-a", help="Include all locally installed repos", action="store_true")
+
+    return parser.parse_args()
+
+### TYPES ###
+def command(s):
+    commands = ["status", "dirty", "install", "list"]
+    if s in commands:
+        return s
+    raise argparse.ArgumentTypeError(s + " not in " + commands)
+
+def valid_file(parser, arg):
+    if not os.path.isfile(arg):
+        parser.error("No file called %s found!" % arg)
+    else:
+        return arg
+
+def valid_dir(parser, arg):
+    if not os.path.isdir(arg):
+        parser.error("No directory called %s found!" % arg)
+    else:
+        return arg
+
+def valid_path(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("No file or directory called %s found!" % arg)
+    else:
+        return arg
+
+def get_config_files():
+    path = os.path.expanduser(CONFIG_DIR)
+    files = [path+f for f in os.listdir(path)]
+    return files
+
+def process_args(args):
+
+    if not (args.repos or args.files or args.a):
+        return process_files(get_config_files())
+
+    repos = list()
+    missing = list()
+
+    if args.repos:
+        l_repos, l_missing = get_repos_from_string_list(args.repos)
+        repos += l_repos
+        missing += l_missing
+
+    if args.files:
+        l_repos, l_missing = process_files(args.files)
+        repos += l_repos
+        missing += l_missing
+
+    if args.a:
+        l_repos = get_all_repos_from_local()
+        repos += l_repos
+
+    return repos, missing
+
+def get_repos_from_string_list(paths):
+    repos = list()
+    missing = list()
+
+    for path in paths:
+        l_repos, l_missing = get_repo_or_string(path)
+        repos += l_repos
+        missing += l_missing
+
+    return repos, missing
+
+def get_all_repos_from_local():
+    command = "find ~ -name '.git' | grep -v /.vim | grep -v /.local"
+    result = run_command(command)
+    repos = list()
+    for line in result:
+        try:
+            repos.append(Repo(line.strip().decode("utf-8")+'/..'))
+        except:
+            pass
+    return repos
+
+def run_command(command):
+    p = subprocess.Popen(command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT)
+    return iter(p.stdout.readline, b'')
+
+def get_all_repos_from_remote():
+    command = "ls "+REMOTE_PROJECT_DIR
+    return run_ssh(command, hide=True).stdout.strip().splitlines()
+
+def get_repo_or_string(path):
+    try:
+        return [Repo(path)], []
+    except:
+        return [], [path]
+
+def process_line(line):
+    try:
+        s = [x for x in line.split(' ') if x]
+        path = os.path.expanduser(s[0])
+        if path[0] not in ['/', '~']:
+            log_error(path + " is not absolute", file=sys.stderr)
+            return None
+        if len(s) == 2:
+            remote = s[1]
+        else:
+            raise
+
+        if os.path.isdir(path):
+            try:
+                return Repo(path)
+            except:
+                if os.listdir(path):
+                    log_error("path exists but is not a valid git directory and is not empty")
+                    return None
+                else:
+                    "Path exists but is empty"
+                    return path
+        else:
+            "path doesnt exist"
+            return path
+    except:
+        log_error("Couldn't process line")
+        return None
+
+def process_files(files=None):
+    processed_lines = list()
+    for f in files:
+        if not os.path.isfile(f):
+            log_error("Not a file:", f)
+            exit(1)
+        with open(f, 'r') as conf:
+            processed_line = [process_line(l.strip()) for l in conf]
+            processed_lines += [x for x in processed_line if x ]
+
+    repos   = [x for x in processed_lines if type(x) is not str]
+    missing = [x for x in processed_lines if type(x) is str]
+
+    return repos, missing
+
+
+### General helpers
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+### Function without side effects
+
+def exists_remote(remote_path):
+    ans = run_ssh('[ -d "'+remote_path+'" ]')
+    if ans.return_code is 0:
+        return True
+    else:
+        return False
+
+def exists_local(path):
+    return os.path.isdir(path)
+
+def is_local_git(path):
+    if not exists_local:
+        return False
+    try:
+        _ = Repo(path).git_dir
+        return True
+    except InvalidGitRepositoryError:
+        return False
+
+### Functions with side effects
+
+### Local edits
+
+def init_local(path, remote_path=None, ):
+    abspath = os.path.abspath(path)
+    if not exists_local(path):
+        print("Creating new local directory:", abspath)
+        os.mkdir(path)
+    # if is_local_git(path):
+    #     print("Local repo exists:", abspath)
+    #     return
+    repo = Repo.init(path)
+    if remote_path:
+        if "origin" not in repo.remotes:
+            print("Adding remote:", remote_path)
+            origin = repo.create_remote("origin", remote_path)
+            origin.fetch()
+        else:
+            log_error("Remote origin exists for", abspath)
+
+
+def install_local(path, username=USERNAME, remote=REMOTE, remote_project_dir=REMOTE_PROJECT_DIR):
+    pass
+
+
+### REMOTE edits
+
+def run_ssh(*args, username=USERNAME, remote=REMOTE, **kwargs):
+    with Connection(remote, user=username) as ssh:
+        return ssh.run(*args, warn=True, **kwargs)
+
+def init_remote(project_name, username=USERNAME, remote=REMOTE, remote_project_dir=REMOTE_PROJECT_DIR):
+    remote_path = os.path.join(remote_project_dir, project_name)
+    remote_url = username+"@"+remote+":"+remote_path
+    if exists_remote(remote_path):
+        # print("Remote folder already exists, not creating remote repo")
+        return remote_url
+    print("Creating remote: " + remote_url)
+    if not run_ssh('mkdir ' + remote_path, username=username, remote=REMOTE):
+        log_error("Couldn't create folder, check permissions")
+        raise
+    if not run_ssh('git init --bare ' + remote_path, username=username, remote=REMOTE):
+        log_error("Created folder but could not init repo, manual action required")
+        raise
+    return remote_url
+
