88d6bf043027862668592c4a760f93823e032d43
wessel.blokzijl <wessel.blokzijl@huntandhackett.com>
2021-12-22 15:39:06 +0100

    Make update multi-repo

--- mgit/ui/commands/update.py
+++ mgit/ui/commands/update.py
@@ -1,4 +1,4 @@
-from mgit.ui.base_commands  import SingleRepoCommand
+from mgit.ui.base_commands  import MultiRepoCommand
 from mgit.ui.commands._mgit import MgitCommand
 from mgit.ui.cli_utils      import query_yes_no
 
@@ -13,7 +13,7 @@ from typing import *
 T = TypeVar('T')
 
 @MgitCommand.register
-class CommandUpdate(SingleRepoCommand):
+class CommandUpdate(MultiRepoCommand):
     command = "update"
     help="Updates the config based on the system or vice versa"
 
@@ -25,27 +25,31 @@ class CommandUpdate(SingleRepoCommand):
 
         parser.add_argument("-y", help="Skip asking for confirmation", action='store_true')
 
-    def run(self, config_state, system_state, config, system, y):
-        state_merger = StateMerger(self.config)
-        if not (config or system): # if none then both
-            config = True
-            system = True
-        repo_state = state_merger.merge(config_state, system_state)
-        if config:
-            print(f"old config={config_state.represent()}")
-        if system:
-            print(f"old system={system_state.represent()}")
-        if not y and not query_yes_no(
-                "Do you want to update with the following values: \n" +
-                f"name={repo_state.represent()}"):
-            return "Doing nothing"
+    def run(self, repo_states, config, system, y):
+        for config_state, system_state in repo_states:
+            if not config_state.compare(system_state):
+                continue
+            state_merger = StateMerger(self.config)
+            if not (config or system): # if none then both
+                config = True
+                system = True
+            repo_state = state_merger.merge(config_state, system_state)
+            if not y:
+                if config:
+                    yield f"old config={config_state.represent()}"
+                if system:
+                    yield f"old system={system_state.represent()}"
+                if not query_yes_no(
+                        "Do you want to update with the following values: \n" +
+                        f"name={repo_state.represent()}"):
+                    return f"Doing nothing for {repo_state.name}"
 
-        if config and system:
-            self.config.set_state(repo_state)
-            self.system.set_state(repo_state)
-        elif system:
-            self.system.set_state(repo_state)
-        elif config:
-            self.config.set_state(repo_state)
-        return repo_state
+            if config and system:
+                self.config.set_state(repo_state)
+                self.system.set_state(repo_state)
+            elif system:
+                self.system.set_state(repo_state)
+            elif config:
+                self.config.set_state(repo_state)
+            yield repo_state
 
